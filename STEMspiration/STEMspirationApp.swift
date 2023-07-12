c7eb099f2df37aefa9e8dfde51216ee13e50abfb
triforcepotato <prieshajhala@gmail.com>
2023-07-12 15:02:11 -0700

    Initial Commit

---
+++ STEMspiration/STEMspirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMspirationApp.swift
+//  STEMspiration
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct STEMspirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
