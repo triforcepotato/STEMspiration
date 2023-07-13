222aa73dd545aeddff7d5623d7e3d18c0243dcc3
triforcepotato <prieshajhala@gmail.com>
2023-07-13 10:02:55 -0700

    Finished project!

--- STEMspiration/ContentView.swift
+++ STEMspiration/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Color(.systemOrange)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                   
+                   Image("katherinejohnson")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .cornerRadius(15)
+                   
+                   HStack {
+                       
+                       Text("Katherine Johnson")
+                           .font(.title)
+                           .fontWeight(.bold)
+                       
+                       Text ("Pioneer of Space Exploration")
+                       
+                   }
+                   
+                   Text("Worked as a human computer at NASA for over 30 years and helped send the first humans to space.")
+               }
+            .padding()
+            .background(Rectangle() .foregroundColor(.white))
+            
+                .cornerRadius(15)
+            
+                .shadow(radius: 15)
+                .padding()
+            
+        
+            
         }
-        .padding()
+        
+   
+                
     }
 }
 
